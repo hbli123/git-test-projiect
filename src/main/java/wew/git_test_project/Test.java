b8bea00f535a49a379c01fac7f9e3bca342ef04d
hbli123 <hbli123@sina.com>
2017-08-07 19:37:56 +0800

    Update Test.java
    test

--- src/main/java/wew/git_test_project/Test.java
+++ src/main/java/wew/git_test_project/Test.java
@@ -3,7 +3,7 @@ package wew.git_test_project;
 public class Test {
 	private static void psvm() {
 		// TODO Auto-generated method stub
-		System.out.println("Hello 58DaoJia");
+		System.out.println("Hello 五八");
 	}
 
 	public static void main(String args[]) {
