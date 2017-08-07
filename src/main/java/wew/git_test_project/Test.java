a117330b104ec8375ddceea754157d327854e8b5
lhb <hbli123@sina.com>
2017-08-07 19:33:58 +0800

    update

--- src/main/java/wew/git_test_project/Test.java
+++ src/main/java/wew/git_test_project/Test.java
@@ -1,5 +1,12 @@
 package wew.git_test_project;
 
 public class Test {
+	private static void psvm() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello 58DaoJia");
+	}
 
+	public static void main(String args[]) {
+		psvm();
+	}
 }
