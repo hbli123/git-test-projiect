c7def7673fee6ad8a0c94ab251af679169484578
lhb <hbli123@sina.com>
2017-08-07 20:07:44 +0800

    处理冲突

--- src/main/java/wew/git_test_project/Test.java
+++ src/main/java/wew/git_test_project/Test.java
@@ -1,12 +1,15 @@
 package wew.git_test_project;
 
 public class Test {
-	private static void psvm() {
+	private static void psvm(String str) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 五八");
+		System.out.println(str);
+		System.out.println(str);
+		System.out.println(str);
 	}
 
 	public static void main(String args[]) {
-		psvm();
+		psvm("nihao");
 	}
 }
