57c1108e3d7dac77b8a54c0f14298be55d420349
hbli123 <hbli123@sina.com>
2017-08-10 10:58:10 +0800

    Update Test.java
    增加冲突情况

--- src/main/java/wew/git_test_project/Test.java
+++ src/main/java/wew/git_test_project/Test.java
@@ -3,10 +3,8 @@ package wew.git_test_project;
 public class Test {
 	private static void psvm(String str) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello 五八");
-		System.out.println(str);
-		System.out.println(str);
-		System.out.println(str);
+		System.out.println("Hello 到家");
+		 
 	}
 
 	public static void main(String args[]) {
